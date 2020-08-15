c831b0f798dc46890962004ca16d4ec816c24064
Nuntipat Pantarag <6110110238@psu.ac.th>
2020-08-15 15:30:03 +0700

    commit 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+var word = "";
+var randWord = Math.floor(Math.random() * 6);
+
+switch (randWord) {
+  case 0: word = "Hello"; break;
+  case 1: word = "Black"; break;
+  case 2: word = "Pink"; break;
+  case 3: word = "Space"; break;
+  case 4: word = "Super"; break;
+  case 5: word = "Market"; break;
+}
+
 function App() {
   return (
     <div>
